95a634fbbff8f6476ac569211c9a254654cce30b
seoyeon-00 <84628128+seoyeon-00@users.noreply.github.com>
2023-03-22 22:17:22 +0900

    Update 가장 가까운 같은 글자.js

--- 230322/가장 가까운 같은 글자.js
+++ 230322/가장 가까운 같은 글자.js
@@ -1,28 +1,21 @@
 //가장 가까운 같은 글자
-//제출시 오류 떠서 오류 잡고 재업로 하겠습니다.
 
 function solution(s) {
     var answer = [];
+    var arr = [];
     let arrS = s.split("");
     
     for(let i in arrS){
-        if(!answer.includes(arrS[i])){
-            answer.push(arrS[i]);
+        if(!arr.includes(arrS[i])){
+            arr.push(arrS[i]);
+            answer.push(-1);
+   
         } else {
-            for(let j = i - 1; j > 0; j--){
-                if(arrS[j] === arrS[i]){                
-                    answer.push(i-j);
-                    break;
-                }
-            }
-
+            answer.push(i - arr.lastIndexOf(arrS[i]));
+            arr.push(arrS[i]);
         }
     }
     
-    answer = answer.join(" ").replace(/[a-z]/g,"-1").split(" ");
-    const arrNum = answer.map((v) => {
-        return Number(v);
-    })
-    
-    return arrNum;
+
+    return answer;
 }
