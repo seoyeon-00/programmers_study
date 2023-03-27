1c8f7bf7f53a029eb34a6f26f74d45ce68c249bb
ksy <reading94@naver.com>
2023-03-27 22:04:45 +0900

    230327 programmers

--- 230327_/문자 반복 출력하기.js
+++
@@ -1,9 +0,0 @@
-function solution(my_string, n) {
-    var answer = [];
-    
-    for(let i = 0; i < my_string.length; i++){
-        answer.push(my_string[i].repeat(n));
-    }
-    
-    return answer.join("");
-}
